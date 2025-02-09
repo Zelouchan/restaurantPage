173836cec12f5c0bca16aafff00a594a9072e08a
Zelouchan <bad1dje@gmail.com>
2025-02-09 15:38:11 +0000

    done

--- src/index.js
+++ src/index.js
@@ -4,21 +4,26 @@ import {mainStructure} from './framework.js'
 import {menu} from './menu.js'
 import {about} from './contact.js'
 
+let content = document.getElementById("content");
 
-// document.getElementById("home").addEventListener("click", function() {
-//   main();
-// });
-
-// document.getElementById("menu").addEventListener("click", function() {
-//   menu();
-// });
-
-// document.getElementById("about").addEventListener("click", function() {
-//   about();
-// });
-
-
-window.addEventListener("load", function () {
-  mainStructure();
+function selectTab() {
+document.getElementById("home").addEventListener("click", function() {
+  content.innerHTML='';
   main();
 });
+
+document.getElementById("menu").addEventListener("click", function() {
+  content.innerHTML='';
+  menu();
+});
+
+document.getElementById("about").addEventListener("click", function() {
+  content.innerHTML='';
+  about();
+});
+}
+
+mainStructure();
+selectTab();
+
+
