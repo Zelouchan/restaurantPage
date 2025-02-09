83de8052c9f96c18d4ae769b2eccb00215805fb3
Zelouchan <bad1dje@gmail.com>
2025-02-09 13:03:10 +0000

    Finished Menu Tab

--- src/contact.js
+++ src/contact.js
@@ -1,10 +1,7 @@
-
+export function about() {
 function active() {
     document.querySelectorAll(".link").forEach(el => el.classList.remove("active"));
     document.getElementById("about").classList.add("active");  
 };
-
-window.addEventListener("load", function () {
-  mainStructure();
-  active();
-});
+active();
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import "./styles.css";
 import { main } from './main.js';
 import {mainStructure} from './framework.js'
 import {menu} from './menu.js'
+import {about} from './contact.js'
 
 
 window.addEventListener("load", function () {
   mainStructure();
-menu();
+  about();
+// menu();
 //   main();
 });
