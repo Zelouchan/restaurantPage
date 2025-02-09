4cae55b8421011599a89584d664f8b10a0d80534
Zelouchan <bad1dje@gmail.com>
2025-02-09 17:19:32 +0000

    Fixed front page

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 import "./styles.css";
-import { main } from './main.js';
-import {mainStructure} from './framework.js'
-import {menu} from './menu.js'
-import {about} from './contact.js'
+import { main } from "./main.js";
+import { mainStructure } from "./framework.js";
+import { menu } from "./menu.js";
+import { about } from "./contact.js";
 
 let content = document.getElementById("content");
 
 function selectTab() {
-document.getElementById("home").addEventListener("click", function() {
-  content.innerHTML='';
-  main();
-});
+  document.getElementById("home").addEventListener("click", function () {
+    content.innerHTML = "";
+    main();
+  });
 
-document.getElementById("menu").addEventListener("click", function() {
-  content.innerHTML='';
-  menu();
-});
+  document.getElementById("menu").addEventListener("click", function () {
+    content.innerHTML = "";
+    menu();
+  });
 
-document.getElementById("about").addEventListener("click", function() {
-  content.innerHTML='';
-  about();
-});
+  document.getElementById("about").addEventListener("click", function () {
+    content.innerHTML = "";
+    about();
+  });
 }
 
 mainStructure();
 selectTab();
-
-
+main();
